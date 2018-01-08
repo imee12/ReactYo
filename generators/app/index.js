22e53bce714c8eed2022d63a2d992818147791c3
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2018-01-08 10:09:34 -0500

    updaing generators/app/index.js in wrting(){} method to make app initialize gitignore,  the styles directory, the test directory, the testSetup.js file; all of which were not previously generated during app-start using yo react

--- generators/app/index.js
+++ generators/app/index.js
@@ -26,13 +26,7 @@ module.exports = class extends Generator {
 
   prompting() {
     // Have Yeoman greet the user.
-    this.log(
-      yosay(
-        'Welcome to Imees Practice  ' +
-          chalk.red('generator-react') +
-          ' generator!',
-      ),
-    );
+    this.log(yosay(`Welcome to Imees Practice ${chalk.red('generator-react')} generator!`));
 
     const prompts = [
       {
@@ -65,7 +59,7 @@ module.exports = class extends Generator {
       this.destinationPath('package.json'),
       {
         name: this.props.name,
-      },
+      }
     );
 
     this.fs.copyTpl(
@@ -73,51 +67,78 @@ module.exports = class extends Generator {
       this.destinationPath('bower.json'),
       {
         name: this.props.name,
-      },
+      }
     );
+
     this.fs.copy(
       this.templatePath('babelrc'),
-      this.destinationPath('.babelrc'),
+      this.destinationPath('.babelrc')
     );
+
     this.fs.copy(
-      this.templatePath('bowerrc'),
-      this.destinationPath('.bowerrc'),
+      this.templatePath('gitignore'),
+      this.destinationPath('.gitignore')
     );
 
     this.fs.copy(
       this.templatePath('_webpack.config.js'),
-      this.destinationPath('webpack.config.js'),
+      this.destinationPath('webpack.config.js')
     );
+
+    this.fs.copy(
+      this.templatePath('bowerrc'),
+      this.destinationPath('.bowerrc')
+    );
+
     this.fs.copy(
       this.templatePath('_index.html'),
-      this.destinationPath('index.html'),
+      this.destinationPath('index.html')
     );
 
     this.fs.copy(
       this.templatePath('_history/_configureHistory.js'),
-      this.destinationPath('src/history/configureHistory.js'),
+      this.destinationPath('src/history/configureHistory.js')
     );
 
     this.fs.copy(
       this.templatePath('_index.js'),
-      this.destinationPath('src/index.js'),
+      this.destinationPath('src/index.js')
     );
 
     this.fs.copy(
       this.templatePath('_store/_configureStore.js'),
-      this.destinationPath('src/store/configureStore.js'),
+      this.destinationPath('src/store/configureStore.js')
+    );
+
+    this.fs.copy(
+      this.templatePath('_tests/_App.spec.js'),
+      this.destinationPath('test/App.spec.js')
+    );
+
+    this.fs.copy(
+      this.templatePath('_testSetup.js'),
+      this.destinationPath('testSetup.js')
+    );
+
+    this.fs.copy(
+      this.templatePath('eslintrc.js'),
+      this.destinationPath('.eslintrc.js')
     );
 
     this.fs.copy(
       this.templatePath('_App.jsx'),
-      this.destinationPath('src/components/App.jsx'),
+      this.destinationPath('src/components/App.jsx')
     );
 
     this.fs.copy(
       this.templatePath('_reducers/_index.js'),
-      this.destinationPath('src/reducers/index.js'),
+      this.destinationPath('src/reducers/index.js')
     );
 
+    this.fs.copy(
+      this.templatePath('_scss/_index.scss'),
+      this.destinationPath('src/scss/index.scss')
+    );
   }
 
   initializing() {
